4022cb53f59a4aecb7e7d75e9c8fed35681f1285
SimonePasetto <simone.pasetto@amilon.eu>
2023-04-19 11:03:15 +0200

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 import './assets/main.css'
 
 const app = createApp(App)
